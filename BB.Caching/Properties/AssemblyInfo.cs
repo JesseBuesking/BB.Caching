c1552fe588308a405bbc9abb637c1f5e3fb582f4
JesseBuesking <jessebuesking@gmail.com>
2015-03-02 13:19:11 -0800

    stylecop'ing lua scriptloader

--- BB.Caching/Properties/AssemblyInfo.cs
+++ BB.Caching/Properties/AssemblyInfo.cs
@@ -5,50 +5,24 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("BB.Caching")]
 [assembly: AssemblyDescription("Simple caching for .NET applications")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BB.Caching")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 #if (DEBUG || TEST)
-// Generates the PublicKey
-// C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC>sn -Tp C:\Repositories\BB
-// \BB.Caching\BB.Caching.Tests\bin\Debug\BB.Caching.Tests.dll
-
-//[assembly: InternalsVisibleTo("BB.Caching.Tests, PublicKey=" +
-//    "0024000004800000940000000602000000240000525341310004000001000100a71c48bf8c5e13" +
-//    "38468d891c5dd3f7e97a2fbe77fdc621c360278316f25ea72515e970ac7264ec7e8c0423aec151" +
-//    "6e228aa044a2ef0700e01625d4b58df994301c6bb0d81a389cad2987a4eff722f6de17e2d19383" +
-//    "ec36c9c97c07435778cd5e249fc439fc651ce46e1b701a74d5090d3684b23ca81e07b012e4ca4c" +
-//    "89167dcc"
-//    )]
 [assembly: InternalsVisibleTo("BB.Caching.Tests")]
 #endif
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("ffa0c3e2-9e8a-4869-94fe-5727de90e50c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("0.0.0.2")]
-[assembly: AssemblyFileVersion("0.0.0.2")]
+[assembly: AssemblyVersion("0.0.0.3")]
+[assembly: AssemblyFileVersion("0.0.0.3")]
--- BB.Caching/Redis/Lua/ScriptLoader.cs
+++ BB.Caching/Redis/Lua/ScriptLoader.cs
@@ -1,37 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading;
-
-namespace BB.Caching.Redis.Lua
+﻿namespace BB.Caching.Redis.Lua
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading;
+
+    /// <summary>
+    /// Manages loading lua scripts from files.
+    /// </summary>
     public class ScriptLoader
     {
-        private static readonly Lazy<ScriptLoader> _lazy = new Lazy<ScriptLoader>(
+        /// <summary>
+        /// Lazily loads the instance.
+        /// </summary>
+        private static readonly Lazy<ScriptLoader> _Lazy = new Lazy<ScriptLoader>(
             () => new ScriptLoader(), LazyThreadSafetyMode.ExecutionAndPublication);
 
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
         public static ScriptLoader Instance
         {
-            get { return _lazy.Value; }
+            get { return _Lazy.Value; }
         }
 
+        /// <summary>
+        /// A cache to store the contents of the lua files.
+        /// </summary>
         private readonly Dictionary<string, string> _cache = new Dictionary<string, string>();
 
+        /// <summary>
+        /// Index to get a script for a particular key.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <returns>
+        /// The lua script contents as a string.
+        /// </returns>
         public string this[string key]
         {
             get
             {
                 string result;
                 if (this._cache.TryGetValue(key, out result))
+                {
                     return result;
+                }
 
                 this._cache[key] = ScriptLoader.LuaFileToString(key);
                 return this._cache[key];
             }
         }
 
+        /// <summary>
+        /// Prevents a default instance of the <see cref="ScriptLoader"/> class from being created.
+        /// </summary>
         private ScriptLoader()
         {
         }
@@ -39,23 +65,33 @@ namespace BB.Caching.Redis.Lua
         /// <summary>
         /// Returns the lua file's contents as a string.
         /// </summary>
-        /// <param name="fileName"></param>
-        /// <returns></returns>
+        /// <param name="fileName">
+        /// The filename of the lua script.
+        /// </param>
+        /// <returns>
+        /// The lua script contents as a string.
+        /// </returns>
         private static string LuaFileToString(string fileName)
         {
             var assembly = Assembly.GetExecutingAssembly();
 
-            string luaResource = String.Format("BB.Caching.Redis.Lua.Scripts.{0}.lua", fileName);
+            string luaResource = string.Format("BB.Caching.Redis.Lua.Scripts.{0}.lua", fileName);
             if (!assembly.GetManifestResourceNames().Contains(luaResource))
+            {
                 throw new ArgumentException(string.Format("Requested resource {0} was not found", luaResource));
+            }
 
             using (var resourceStream = assembly.GetManifestResourceStream(luaResource))
             {
                 if (null == resourceStream)
+                {
                     throw new ArgumentException(string.Format("Unable to get resource stream for {0}", luaResource));
+                }
 
                 using (var stream = new StreamReader(resourceStream))
+                {
                     return stream.ReadToEnd();
+                }
             }
         }
     }
